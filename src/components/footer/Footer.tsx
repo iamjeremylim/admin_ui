e42e29d076888a9f27f0357e77f14b1513672663
jeremylim <Jeremy.lim@ntuclink.com.sg>
2023-07-19 17:53:15 +0800

    feat: navbar, menu, footer, home content with analytics styled with grid

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,12 @@
 import "./footer.scss";
 
 function Footer() {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <span>AdminUI</span>
+      <span>Jeremy's Admin Dashboard</span>
+    </div>
+  );
 }
 
 export default Footer;
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,7 +1,28 @@
 import "./navbar.scss";
 
 function NavBar() {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="" />
+        <span>AdminUI</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="/notifications.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src="/profile.jpg" alt="" />
+          <span>Jeremy</span>
+        </div>
+        <img src="/settings.svg" alt="" className="icon" />
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
